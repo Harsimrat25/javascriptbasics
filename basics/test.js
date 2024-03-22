f3762914e4c962548e3b9eb983e409405deff403
Harsimrat25 <97719623+Harsimrat25@users.noreply.github.com>
2024-03-22 10:43:46 +0000

    data type conversion

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,10 @@
 console.log("harsimrat")
+const name="sehaj";
+let email="simrat@gmail.com";
+var password="abc";
+let city="Chandigarh";
+let city="bengaluru";
+console.table=([email],[city],[password]);
+//js didnt know about scope that's why let was used instead of var (use var preferable)
+let state="punjab";
+let song;//this will be shown undefined
---
+++ basics/variable.js
@@ -0,0 +1,12 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)  in node it will show error
+
+//data types
+let name="sehaj"
+let age=14
+let class=9
+let singer=false
+// number bigint string null bool undefined signal (in rect)->unique
+
+console.log(typeof null) // ->object
+console.log(typeof undefined) //->undefined
