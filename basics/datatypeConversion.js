4f9d46e292a48c96efb7e991bd4d571370ac87ac
Harsimrat25 <97719623+Harsimrat25@users.noreply.github.com>
2024-03-22 10:27:20 +0000

    datatype conversion

---
+++ basics/datatypeConversion.js
@@ -0,0 +1,19 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof(score));//passed as a method
+
+let scoreInNumber=Number(score);
+console.log(typeof scoreInNumber);
+let santa="33abc"
+console.log(typeof santa);
+let santaInNumber=Number(santa)
+console.log(santaInNumber);
+
+//operaions
+let num=20
+let negnum=-num
+console.log(negnum);
+
+console.log("1"+2)
+console.log("1"+"2"+1)
+console.log(1+3+"2"+2);
